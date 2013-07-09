d1f81d58ebff1352cef22a0862f741f86915ec55
Øystein Skartsæterhagen <oysteini@math.ntnu.no>
2013-07-09 11:44:30 +0200

    Removed old cat code and moved chainmap code to chainmap.g{d,i}.

--- lib/complex.gd
+++ lib/complex.gd
@@ -1,15 +1,3 @@
-DeclareCategory( "IsCat", IsRecord );
-DeclareRepresentation( "IsCatDefaultRep", IsComponentObjectRep,
-                       [ "identity", "properties" ] );
-
-#DeclareOperation( "\.", [ IsCat, IsString ] );
-
-DeclareGlobalFunction( "Cat" );
-
-DeclareOperation( "CatOfRightAlgebraModules", [ IsAlgebra ] );
-
-
-
 DeclareCategory( "IsComplex", IsObject );
 DeclareCategoryFamily( "IsComplex" );
 
@@ -71,31 +59,3 @@ DeclareGlobalFunction( "ShortExactSequence" );
 DeclareGlobalFunction( "ComplexByDifferentialList" );
 
 DeclareOperation( "ProjectiveResolution", [ IsAlgebraModule ] );
-
-DeclareCategory( "IsChainMap", IsObject );
-DeclareCategoryFamily( "IsChainMap" );
-
-DeclareRepresentation( "IsChainMapDefaultRep",
-                       IsComponentObjectRep and IsAttributeStoringRep,
-                       [] );
-
-DeclareAttribute( "Source", IsChainMap );
-DeclareAttribute( "Range", IsChainMap );
-DeclareAttribute( "MorphismsOfChainMap", IsChainMap );
-DeclareOperation( "MorphismOfChainMap", [ IsChainMap, IsInt ] );
-DeclareOperation( "\^", [ IsChainMap, IsInt ] );
-
-DeclareOperation( "HighestKnownDegree", [ IsChainMap ] );
-DeclareOperation( "LowestKnownDegree", [ IsChainMap ] );
-
-DeclareGlobalFunction( "ChainMap" );
-DeclareGlobalFunction( "FiniteChainMap" );
-DeclareGlobalFunction( "ZeroChainMap" );
-
-DeclareGlobalFunction( "ComplexAndChainMaps" );
-
-# ComplexAndChainMaps( sourceComplexes, rangeComplexes,
-#                      basePosition, middle, positive, negative );
-
-# ComplexAndChainMaps( [ C ], [], 0, [ [ g, f ] ], [ "pos", func ], "zero" );
-
